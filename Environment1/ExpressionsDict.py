1ce94f35d6e92b5121495eeae5a129c62fa26342
marpa96 <marpab96@gmail.com>
2021-11-20 12:30:26 -0500

    Added diverse expression dictionary

---
+++ Environment1/ExpressionsDict.py
@@ -0,0 +1,22 @@
+
+Expressions = {
+    'spoke_with_single': [
+        'I spoke with ',
+        'I was able to speak directly with ',
+    ],
+    'customer_called_single': [
+        ' called us'
+    ],
+    'spoke_with_multiple': [
+        'I was able to speak with the following customers on the phone: ',
+        'I called and spoke directly with the following customers: ',
+        'I was a able to get the following customers on the phone: '
+    ],
+    'left_message_multiple': [
+        'I was unable to speak directly with the following customer(s), but I left them a message: '
+    ],
+    'unable_to_contact': [
+        'Tried reaching out to but was unable to get directly in touch with ',
+        'Received no response from '
+    ],
+}
